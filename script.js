e2ec12743cc856094a94857ae8eba2beb20ed537
rafaelb056 <rafaelbenitez@gmail.com>
2024-08-07 12:16:25 -0400

    add .vscode

--- script.js
+++ script.js
@@ -22,12 +22,12 @@ h3.textContent = "I'm a blue h3!";
 
 container.appendChild( h3 );
 
-const div = document.createElement("div");
+const divb = document.createElement("div");
 
-div.style.border = "1px solid black";
-div.style.backgroundColor ="pink";
+divb.style.border = "1px solid black";
+divb.style.backgroundColor ="pink";
 
-container.appendChild(div);
+container.appendChild(divb);
 
 const h1 = document.createElement("h1");
 const p2  = document.createElement("p");
@@ -35,5 +35,5 @@ const p2  = document.createElement("p");
 h1.textContent = "I'm in a div";
 p2.textContent = "ME TOO!";
 
-div.appendChild(h1);
-div.appendChild(p2);
+divb.appendChild(h1);
+divb.appendChild(p2);
